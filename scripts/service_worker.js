557701edaf13cc1d8cefbdfae45ffafba33c2d85
Brian Thomas <brianjamesthomas@live.com>
2021-02-24 08:51:49 +0000

    Set up the basic progressive web application structure

---
+++ scripts/service_worker.js
@@ -0,0 +1,13 @@
+// Set up the service worker to make the app installable
+// Information can set to be made available offline
+
+self.addEventListener("install", function (event) {
+    return self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+    self.clients.claim();
+});
+
+// A fetch event is required to make tha application installable
+self.addEventListener("fetch", function (event) {});
