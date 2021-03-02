6ecf0e33e99e4a5a7584b2839f8515be12b30fa6
Brian Thomas <brianjamesthomas@live.com>
2021-03-02 13:40:18 +0000

    Fix bugs related to the offline operation of the application

--- service_worker.js
+++ service_worker.js
@@ -1,22 +1,19 @@
-// Set up the service worker to make the app installable.
-// Information can be set to be made available offline.
-
 const staticCacheName = "site-static-v1";
 const dynamicCacheName = "site-dynamic-v1";
 const assets = [
     "/",
     "/index.html",
+    // "/scripts/main.js",
+    "/styles/main.css",
     "/favicon.ico",
     "/manifest.json",
     "/browserconfig.xml",
-    "/images/safari-pinned-tab.svg",
-    "/images/mstile-150x150.png",
-    "/images/apple-touch-icon.png",
+    "/images/other_images/apple-touch-icon.png",
+    "/images/other_images/safari-pinned-tab.svg",
+    "/images/other_images/mstile-150x150.png",
     "/images/manifest_icons/icon-192x192.png",
     "/images/manifest_icons/icon-512x512.png",
     "/images/manifest_icons/maskable-icon-256x256.png",
-    "/scripts/main.js",
-    "/styles/main.css",
     "https://use.fontawesome.com/releases/v5.0.13/css/all.css",
     "https://use.fontawesome.com/releases/v5.0.13/webfonts/fa-solid-900.woff2",
     "https://use.fontawesome.com/releases/v5.0.13/webfonts/fa-regular-400.woff2",
@@ -25,26 +22,21 @@ const assets = [
 ];
 
 self.addEventListener("install", (event) => {
-    // return self.skipWaiting();
     event.waitUntil(
         caches.open(staticCacheName).then((cache) => {
-            console.log("Caching shell assets");
             cache.addAll(assets);
         })
     );
 });
 
 self.addEventListener("activate", (event) => {
-    // self.clients.claim();
     event.waitUntil(
         caches.keys().then((keys) => {
-            // console.log(keys);
             return Promise.all(keys.filter((key) => key !== staticCacheName && key !== dynamicCacheName).map((key) => caches.delete(key)));
         })
     );
 });
 
-// A fetch event is required to make tha application installable.
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((cacheRes) => {
